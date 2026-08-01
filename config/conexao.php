64f92bc914dd57408ad59b5e2e7723a7f0638cff
Vitor Maldonado <vitorleandro2201@gmail.com>
2026-08-01 08:33:38 -0300

    Commit inicial do projeto WinCar

---
+++ config/conexao.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$banco = "wincar";
+$usuario = "root";
+$senha = "";
+
+try {
+    $conexao = new PDO(
+        "mysql:host=$host;dbname=$banco;charset=utf8",
+        $usuario,
+        $senha
+    );
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $erro) {
+    die("Erro na conexão: " . $erro->getMessage());
+}
+
+?>
