338e7ee85263974ffa951b56c045db4f0c8ae114
Vitor Maldonado <vitorleandro2201@gmail.com>
2026-08-05 22:17:17 -0300

    mudança da fonte dos textos

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,10 @@ if (session_status() === PHP_SESSION_NONE) {
     <title>WinCar - Sistema de Agendamento</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/estilo.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    
 </head>
 
 <body class="d-flex flex-column min-vh-100 bg-light">
