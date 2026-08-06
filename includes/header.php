d98e64b2bd4a9b57b6949ba428eb91086ceb5d75
Vitor Maldonado <vitorleandro2201@gmail.com>
2026-08-05 22:37:00 -0300

    adição de fav icon

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WinCar - Sistema de Agendamento</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/estilo.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
