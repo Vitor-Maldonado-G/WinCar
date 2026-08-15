841cd351574764d650b5b7eb18a15ccaba3ed49f
Vitor Maldonado <vitorleandro2201@gmail.com>
2026-08-15 10:36:36 -0300

    Atualiza dump do banco com coluna imagem

--- database/wincar.sql
+++ database/wincar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14/08/2026 às 04:30
+-- Tempo de geração: 15/08/2026 às 15:34
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -44,8 +44,11 @@ CREATE TABLE `agendamento` (
 --
 
 INSERT INTO `agendamento` (`id_agendamento`, `id_cliente`, `id_servico`, `placa`, `modelo`, `data`, `hora`, `status`, `created_at`) VALUES
-(1, 7, 1, 'NEG0244', 'Gol 2.0', '2026-08-06', '17:00:00', 'Cancelado', '2026-08-05 00:53:35'),
-(2, 7, 3, 'NEG0244', 'Fiat Uno', '2026-08-08', '14:00:00', 'Cancelado', '2026-08-08 15:59:54');
+(9, 31, 3, 'ABC1234', 'Classic', '2026-08-10', '10:00:00', 'Confirmado', '2026-08-08 02:35:41'),
+(10, 32, 2, 'ABC1234', 'Toyota Hilux', '2026-08-17', '14:00:00', 'Cancelado', '2026-08-08 15:50:05'),
+(11, 32, 3, 'SWG6767', 'Fiat Strada', '2026-08-16', '13:00:00', 'Pendente', '2026-08-08 16:01:44'),
+(12, 33, 2, 'ABC1234', 'Chevrolet Kadett', '2026-08-31', '15:00:00', 'Confirmado', '2026-08-08 16:08:29'),
+(15, 32, 4, 'ABC1234', 'Honda Civic', '2026-08-31', '10:00:00', 'Cancelado', '2026-08-12 01:38:49');
 
 -- --------------------------------------------------------
 
@@ -67,9 +70,12 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`id_cliente`, `nome`, `email`, `telefone`, `senha`, `tipo_usuario`) VALUES
-(7, 'Emanuel trueadam', 'emanuelteste@gmail.com', '(11) 99999-9999', '$2y$10$3IxkNX0tDIZboIZiR1suoetg2/6NDowQ8RDaqY.GKBI2vERDhvR8O', 'admin'),
-(8, 'Vitor Lindo', 'vitorteste@gmail.com', '(11) 99999-9999', '$2y$10$micIjz58L6eDon.73tzN9.KqtFG6z9ylRyOt3JaoxBUxYlm2UtDem', 'cliente');
+(31, 'Vitor', 'vitor@gmail.com', '(19) 99999-9999', '$2y$10$qOD.fOiUximZGNzLtlRGMuNsDRXa9729dotwWm5X4SyCM2N5CLA2W', 'admin'),
+(32, 'Rei', 'rei@gmail.com', '(11) 99999-9999', '$2y$10$zbbgCNdZ72K6hVui2BAxvuJg5p05YAkUaXQTaXOl8VwoYgrpYVugy', 'cliente'),
+(33, 'Leonardo Almeida', 'leoalmeida@gmail.com', '(19) 98373-8919', '$2y$10$Mv4YVtAEBk0o7khrOlRaJ.lEeskkyunm31Kaui/dwo8Tl0mRbch5y', 'cliente'),
+(34, 'ben@gmail.com', 'ben10@gmail.com', '(19) 91010-1010', '$2y$10$MBoPCjgBpLPq2RD6V0hqyuAc6LCyCVFTLjo/ddqoe0cw3QBDrjBum', 'cliente');
 
+-- --------------------------------------------------------
 
 --
 -- Estrutura para tabela `servico`
@@ -80,18 +86,19 @@ CREATE TABLE `servico` (
   `nome` varchar(100) NOT NULL,
   `descricao` text DEFAULT NULL,
   `preco` varchar(30) NOT NULL,
-  `duracao` int(11) NOT NULL
+  `duracao` int(11) NOT NULL,
+  `imagem` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `servico`
 --
 
-INSERT INTO `servico` (`id_servico`, `nome`, `descricao`, `preco`, `duracao`) VALUES
-(1, 'Lavagem Simples', 'Lavagem externa do veículo.', '40.00', 60),
-(2, 'Lavagem Completa', 'Lavagem interna e externa.', '80.00', 120),
-(3, 'Polimento', 'Polimento da pintura do veículo.', '250.00', 240),
-(4, 'Lavagem de Motor', 'Limpeza do compartimento do motor.', '70.00', 90);
+INSERT INTO `servico` (`id_servico`, `nome`, `descricao`, `preco`, `duracao`, `imagem`) VALUES
+(1, 'Lavagem Simples', 'Lavagem externa do veículo.', '40.00', 60, 'lavando1.jpg'),
+(2, 'Lavagem Completa', 'Lavagem interna e externa.', '80.00', 120, 'lavando2.jpg'),
+(3, 'Polimento', 'Polimento da pintura do veículo.', '250.00', 240, 'polimento1.jpg'),
+(4, 'Lavagem de Motor', 'Limpeza do compartimento do motor.', '70.00', 90, 'lavagemmotor1.webp');
 
 --
 -- Índices para tabelas despejadas
@@ -127,13 +134,13 @@ ALTER TABLE `servico`
 -- AUTO_INCREMENT de tabela `agendamento`
 --
 ALTER TABLE `agendamento`
-  MODIFY `id_agendamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_agendamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT de tabela `servico`
